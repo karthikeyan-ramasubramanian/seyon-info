7b4fb2d6b6f0de91714cb91a8416228ea0fdfef3
Venkatesh S <venkateshvenkatesh90377@gmail.com>
2024-01-18 17:23:37 +0530

    venkat-updated-18

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------------------
-    Template Name: Seyon Info- Creative Agency HTML Template
+    Template Name: Seyon Info-  
     Template URI: https://webtend.net/demo/html/seyon/
     Author: WebTend
     Author URI:  https://webtend.net/
